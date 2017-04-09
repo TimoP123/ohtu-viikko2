ee867b28bd652a648f823e1c6c173c52370e9a06
Timo Pudas <timo.pudas@helsinki.fi>
2017-04-09 21:38:12 +0300

    Vko4 t1 tehty

--- ohtu2017/koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ ohtu2017/koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,40 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "013027630";
+        if (args.length > 0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats2017.herokuapp.com/students/" + studentNr + "/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        System.out.println("json-muotoinen data:");
+        System.out.println(bodyText);
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        int sumExercises = 0;
+        int sumTime = 0;
+        
+        System.out.println("opiskelijanumero: " + subs[0].getStudent_number());
+
+        for (Submission submission : subs) {
+            sumExercises += submission.getDone();
+            sumTime += submission.getHours();
+            System.out.println(submission);
+        }
+        
+        System.out.println("\nyhteensä: " + sumExercises + " tehtävää " + sumTime + " tuntia");
+
     }
 }
--- ohtu2017/koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ ohtu2017/koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,7 +1,26 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
     private String student_number;
+    private int week;
+    private int hours;
+    private int done;
+    private String exercises;
+    private boolean a1;
+    private boolean a2;
+    private boolean a3;
+    private boolean a4;
+    private boolean a5;
+    private boolean a6;
+    private boolean a7;
+    private boolean a8;
+    private boolean a9;
+    private boolean a10;
+    private boolean a11;
+    private boolean a12;
+
 
     public String getStudent_number() {
         return student_number;
@@ -11,9 +30,88 @@ public class Submission {
         this.student_number = student_number;
     }
 
-    @Override
-    public String toString() {
-        return student_number;
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getDone() {
+        calculateExercises();
+        return done;
+    }
+
+    
+    
+    
+    
+    private void calculateExercises() {
+        this.done = 0;
+        this.exercises = "";
+        if(a1) {
+            done++;
+            exercises += "1 ";
+        }
+        if(a2) {
+            done++;
+            exercises += "2 ";
+        }
+        if(a3) {
+            done++;
+            exercises += "3 ";
+        }
+        if(a4) {
+            done++;
+            exercises += "4 ";
+        }
+        if(a5) {
+            done++;
+            exercises += "5 ";
+        }
+        if(a6) {
+            done++;
+            exercises += "6 ";
+        }
+        if(a7) {
+            done++;
+            exercises += "7 ";
+        }
+        if(a8) {
+            done++;
+            exercises += "8 ";
+        }
+        if(a9) {
+            done++;
+            exercises += "9 ";
+        }
+        if(a10) {
+            done++;
+            exercises += "10 ";
+        }
+        if(a11) {
+            done++;
+            exercises += "11 ";
+        }
+        if(a12) {
+            done++;
+            exercises += "12";
+        }   
     }
     
+    @Override
+    public String toString() {
+        calculateExercises();
+        return "Viikko " + week + ": tehtyjä tehtäviä yhteensä: " + done +
+                ", aikaa kului " + getHours() + ", tehdyt tehtävät " + exercises;
+    }
 }
