62cc50a15357f7439deee364a3fd32819bf4fee4
Timo Pudas <timo.pudas@helsinki.fi>
2017-04-09 23:03:36 +0300

    Vko4 t6 tehty

--- ohtu2017/koodi/viikko4/WebLogin/src/main/java/ohtu/Tester.java
+++ ohtu2017/koodi/viikko4/WebLogin/src/main/java/ohtu/Tester.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "../../../../../chromedriver"); 
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
@@ -15,18 +16,54 @@ public class Tester {
         sleep(2);
         
         WebElement element = driver.findElement(By.linkText("login"));
-        element.click();
+        //element.click();
 
         sleep(2);
-
+        /*
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
+        */
+        /*
+        // Skenaario: Oikea tunnus, väärä salasana
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akke");
+        element = driver.findElement(By.name("login"));
+        */
+        /*
+        // Skenaario: Käyttäjänimeä ei ole
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaa");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        */
+        
+        // Skenaario: Uuden käyttäjätunnuksen luominen
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("Heikkii");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("Huihai");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("Huihai");
+        element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
 
         sleep(3);
         
